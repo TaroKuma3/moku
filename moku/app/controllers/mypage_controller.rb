e9edec1c48e897c2a9bfc076796f85f1428464b6
tarokuma3 <rodysan.mojya@gmail.com>
2019-03-07 18:40:36 +0900

    2019/03/07 カレンダーの実装

--- moku/app/controllers/mypage_controller.rb
+++ moku/app/controllers/mypage_controller.rb
@@ -3,8 +3,8 @@ class MypageController < ApplicationController
 
   def index
     @user = current_user
-    # @work_randoms = Work.where(pick_up: true).where(deleted: false).order(created_at: 'desc').limit(3)
-    # @do_mokus = DoMoku.where(user_id: current_user.id).where(deleted: false) #カレンダーのために取得
+    @work_randoms = Work.where(pick_up: true).where(deleted: false).order(created_at: 'desc').limit(3)
+    @do_mokus = DoMoku.where(user_id: current_user.id).where(deleted: false) #カレンダーのために取得
   end
 
   def show

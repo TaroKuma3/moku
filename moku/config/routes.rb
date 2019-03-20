2adc7fb3c3ed5b9614f2cb5739f2976b75c42896
tarokuma3 <rodysan.mojya@gmail.com>
2019-03-20 20:33:07 +0900

    2019/03/20 work/showと利用規約

--- moku/config/routes.rb
+++ moku/config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   get root to: 'top#index'
   get "top/about" => "top#about"
+  get 'new_registration/terms_of_service' => 'top#terms_of_service'
 
   # accounts
   get 'accounts/:user_id' => 'accounts#edit'

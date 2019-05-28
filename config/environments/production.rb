759ebc3a1be9e26c8bafe3d10cff865ea5028e71
tarokuma3 <rodysan.mojya@gmail.com>
2019-05-28 11:47:10 +0900

    #23

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,6 +72,7 @@ Rails.application.configure do
       :enable_starttls_auto => true
   }
 
+  config.action_mailer.default_url_options = { host: 'moku-moku-moku.herokuapp.com', protocol: "https" }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
